8e486f23a4efa84b33a162515d90061a45f456b2
Pedro Cardoso <pmtcardoso@gmail.com>
2024-10-14 10:33:32 +0100

    Change headers

--- next.config.js
+++ next.config.js
@@ -4,10 +4,12 @@ module.exports = {
             {
                 source: '/',
                 destination: 'https://content.goe.menu/',
+                headers: { 'Netlify-Vary': 'query' },
             },
             {
                 source: '/:path*',
                 destination: 'https://content.goe.menu/:path*',
+                headers: { 'Netlify-Vary': 'query' },
             },
         ]
     },
